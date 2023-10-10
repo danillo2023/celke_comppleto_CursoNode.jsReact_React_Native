bc8e1d79be93525f0595f43621266e5260fe40f9
danilloWillian <danwillgon@gmail.com>
2023-10-10 10:31:02 -0300

    dropdown concluido

---
+++ Layout_ADM/layout/scriptADM.js
@@ -0,0 +1,14 @@
+    //Dropdown navbar
+    let avatar = document.querySelector(".avatar"),
+        notification = document.querySelector(".notification");
+
+dropMenu(avatar);
+dropMenu(notification);        
+
+    function dropMenu(selector){
+        selector.addEventListener("click", ()=>{
+            let dropDownMenu = selector.querySelector(".dropdown-menu");
+            dropDownMenu.classList.contains("active")?dropDownMenu.classList.remove("active"):dropDownMenu.classList.add("active");
+        });
+    }
+    
