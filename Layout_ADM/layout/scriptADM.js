0840f8964714ebfbf72abe9cfb54abbad69bfc38
danilloWillian <danwillgon@gmail.com>
2023-10-13 17:06:21 -0300

    curso celke validando formulario via bootstrap

--- Layout_ADM/layout/scriptADM.js
+++ Layout_ADM/layout/scriptADM.js
@@ -1,49 +1,49 @@
-    //Dropdown navbar
-    let avatar = document.querySelector(".avatar"),
-        notification = document.querySelector(".notification");
+//Dropdown navbar
+let avatar = document.querySelector(".avatar"),
+    notification = document.querySelector(".notification");
 
 
 
 dropMenu(avatar);
-dropMenu(notification);        
+dropMenu(notification);
 
-    function dropMenu(selector){
-        selector.addEventListener("click", ()=>{
-            let dropDownMenu = selector.querySelector(".dropdown-menu");
-            dropDownMenu.classList.contains("active")? dropDownMenu.classList.remove("active"):dropDownMenu.classList.add("active");
-        });
-    }
+function dropMenu(selector) {
+    selector.addEventListener("click", () => {
+        let dropDownMenu = selector.querySelector(".dropdown-menu");
+        dropDownMenu.classList.contains("active") ? dropDownMenu.classList.remove("active") : dropDownMenu.classList.add("active");
+    });
+}
 
 //saibedar Toggle
-    let sidebar = document.querySelector(".sidebar"),
-         bars = document.querySelector(".bars");
-    bars.addEventListener("click", () =>{ 
-        sidebar.classList.contains("active") ? sidebar.classList.remove("active") :
+let sidebar = document.querySelector(".sidebar"),
+    bars = document.querySelector(".bars");
+bars.addEventListener("click", () => {
+    sidebar.classList.contains("active") ? sidebar.classList.remove("active") :
         sidebar.classList.add("active")
-    });
-    
-    window.matchMedia("(max-width : 768px)").matches? sidebar.classList.remove("active"): sidebar.classList.add("active");
+});
 
-    //Botao acao do lista
-    function actionDropdown(id){
-        closeDropdownAction();
-        document.getElementById("actionDropdown" + id).classList.toggle("show-dropdown-action");
- }  
-window.onclick = function(event){
+window.matchMedia("(max-width : 768px)").matches ? sidebar.classList.remove("active") : sidebar.classList.add("active");
+
+//Botao acao do lista
+function actionDropdown(id) {
+    closeDropdownAction();
+    document.getElementById("actionDropdown" + id).classList.toggle("show-dropdown-action");
+}
+window.onclick = function (event) {
     if (!event.target.matches('.dropdown-btn-action')) {
-           // document.getElementById("actionDropdown").classList.remove("show-dropdown-action");
-      closeDropdownAction();
+        // document.getElementById("actionDropdown").classList.remove("show-dropdown-action");
+        closeDropdownAction();
     }
 }
 
 function closeDropdownAction() {
     var dropdowns = document.getElementsByClassName("dropdown-action-item");
     var i;
-  // meu erro  (i = 0; 1 < dropdowns.length; i++) {
+    // meu erro  (i = 0; 1 < dropdowns.length; i++) {
     for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i] 
+        var openDropdown = dropdowns[i]
         if (openDropdown.classList.contains("show-dropdown-action")) {
-           openDropdown.classList.remove("show-dropdown-action");
+            openDropdown.classList.remove("show-dropdown-action");
         }
     }
 }
